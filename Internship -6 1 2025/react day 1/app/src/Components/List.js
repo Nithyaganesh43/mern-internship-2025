57b9845e388e4a3b939deb4023cee26061d7ff25
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-01-21 10:16:59 +0530

    d

--- Internship -6 1 2025/react day 1/app/src/Components/List.js
+++ Internship -6 1 2025/react day 1/app/src/Components/List.js
@@ -3,6 +3,9 @@ import './List.css'
 const List = ({ prop }) => {
   return (
     <div className="list">
+      <input 
+      type='checkbox'
+      ></input>
       <p>{prop.value}</p>
       <button onClick={() => prop.Remove(prop.key)}>X</button>
     </div>
--- Internship -6 1 2025/react day 1/app/src/Components/Todo.js
+++ Internship -6 1 2025/react day 1/app/src/Components/Todo.js
@@ -2,9 +2,11 @@ import List from './List';
 import { useState } from 'react';
 import './Todo.css';
 const Todo = () => {
-  const [todoList, settodoList] = useState({});
+  const [todoList, settodoList] = useState(
+    JSON.parse(localStorage.getItem('todo'))
+  );
   const [taskIndex, settaskIndex] = useState(0);
-
+localStorage.setItem('todo', JSON.stringify(todoList));
   const Remove = (ind) => {
     let temp = { ...todoList };
     delete temp[ind];
@@ -14,17 +16,26 @@ const Todo = () => {
     let val = document.getElementById('Entered').value.trim();
     if(val){
       
-      settodoList({ ...todoList, [taskIndex]: val });
+      settodoList({ ...todoList, [taskIndex]:val});
       settaskIndex(taskIndex + 1);
     }
     document.getElementById('Entered').value = '';
   }; 
+
+  document.addEventListener('keypress',(e)=>{ 
+    if (e.code === 'Enter') {
+      document.getElementById('btn').click();
+    } else {
+      document.getElementById('Entered').focus();
+    }
+  })
+
   return (
     <div className='Container'>
       <h1>1+1 Do List</h1>
       <div className={'Bar'}>
         <input id="Entered" placeholder={'Enter Task'}></input>
-        <button onClick={Add}>Add</button>
+        <button id={'btn'} onClick={Add}>Add</button>
       </div>
       <div className="ListItems">
         {Object.entries(todoList).map(([key, value]) => (
